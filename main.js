a2cf0e42798cf6ad12542cf8a1c3b4d9ba1f1ebf
Luis Ernesto Gonzalez <luis.e.gonzalez.m@gmail.com>
2023-05-24 10:42:36 -0300

    Fusion Mobile Version

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 
 menuEmail.addEventListener('click', toggleDestokpMenu);
+burgerIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDestokpMenu() {
     desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+}
+
+
